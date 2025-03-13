bedacb3f830f99f790c0ab2411be283bc51e5b1c
Roman Liutko <dossalab@pm.me>
2025-03-13 18:51:36 +0100

    added a sub function (now with VS code!!!)

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@ int sum(int a, int b)
    return a + b;
 }
 
+int sub(int a, int b)
+{
+   return a - b;
+}
+
 int main()
 {
    int a = 10, b = 12;
 
    std::cout << "hello world!" << std::endl;
    std::cout << "sum of " << a << " and " << b << " is " << sum(a, b) << std::endl;
+   std::cout << "sub of " << a << " and " << b << " is " << sub(a, b) << std::endl;
 
    return 0;
 }
